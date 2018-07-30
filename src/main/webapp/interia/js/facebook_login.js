f3f5a2882ca8bf665fd3398de358ab61051daf2a
berkleylim <berkleylim16@gmail.com>
2018-07-30 17:09:11 +0900

    2018. 7. 30. (월)

--- src/main/webapp/interia/js/facebook_login.js
+++ src/main/webapp/interia/js/facebook_login.js
@@ -1,50 +1,14 @@
 // 로그인 성공한 후 서버에 자동으로 로그인 하기
 // => 페이스북으로부터 사용자 정보 가져오기 (페이스북으로부터 사용자 정보를 가져올 수 있도록 서버에 accessToken을 보낸다.)
 function autoServerLogin(accessToken) {
-	var result = getUserInfo(accessToken);
-//	console.log(result.userId);
-	// 페이스북으로 처음으로 로그인 했을 경우!
-//	$.getJSON(serverRoot + "/json/member/", (data) => {
-//		console.log(data.no);
-//		if (data.no < 1) {
-//			// FB.api('서비스 URL', 서비스 결과를 받았을 때 호출될 함수);
-//			// => '/me' : 현재 로그인 한 사용자의 정보를 가져오는 서비스이다.
-//				no: response.authResponse.accessToken.id
-//				id: String(response.authResponse.accessToken.id),
-//				password: "-",
-//				name: response.authResponse.accessToken.name,
-//				nickname: response.authResponse.accessToken.name,
-//				phoneNumber: "-"
-//			
-//			//$.post(serverRoot + "/json/member/add", data);
-//		    
-//		}
-//	});
-	// 이와 동일하게 로그인 실행
-//	$.post(serverRoot + "/json/auth/login", data, (result) => {
-//        if (result.state == "success") {
-//        	location.href = "../main/index.html";
-//        }
-//        else 
-//            window.alert("로그인 실패!");
-//    }, "json");
-}
-
-//로그인 성공한 후 사용자 정보 가져오기
-//=> 로그인을 하면 페이스북에서 통행권을 발급한다.
-//=> 이 통행권이 있어야 페이스북에 서비스를 요청할 수 있다.
-function getUserInfo(accessToken) {
- // 자바스크립트에서 페이스북에 서비스를 요청할 때는 
- // accessToken을 따로 지정하지 않아도 된다.
- // 왜? 
- // => 서비스를 요청할 때 내부에 보관된 accessToken을 사용할 것이다.
- 
- // FB.api('서비스 URL', 서비스 결과를 받았을 때 호출될 함수);
- // => '/me' : 현재 로그인 한 사용자의 정보를 가져오는 서비스이다.
- FB.api('/me?fields=id,name,email', function(response) {
-     console.log(response);
-     //autoServerLogin(response.authResponse.accessToken);
- });
+    	$.getJSON(serverRoot + '/json/auth/facebookLogin', 
+			{'accessToken': accessToken}, function(result) {
+			if (result.status == 'success') {
+				location.href = '../main/index.html';
+			} else {
+				window.alert('로그인 실패입니다!');
+			}
+		});
 }
 
 //페이스북 로그인을 수행한 후에 그 결과에 따라 작업을 수행한다.
@@ -52,9 +16,7 @@ function statusChangeCallback(response) {
   //console.log(response);
 
   if (response.status === 'connected') { // 로그인이 정상적으로 되었을 때,
-	  //console.log(response.authResponse.accessToken)
       autoServerLogin(response.authResponse.accessToken);
-	  //getUserInfo(response.authResponse.accessToken); // 테스트용
   } else { // 로그인이 되지 않았을 때,
       console.log("로그인 되지 않았음");
   }
